95109cf17a56271833d3809e975b76a7d60a5ae7
Asarck <85520089+Asarck@users.noreply.github.com>
2022-03-18 09:29:31 +0100

    modif header

--- www/Header.php
+++ www/Header.php
@@ -19,16 +19,18 @@
         </a>
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-4 link-secondary">Accueil</a></li>
-            <li><a href="#" class="nav-link px-4 link-dark">Recherche de stage</a></li>
-            <li><a href="#" class="nav-link px-4 link-dark">Mes candidatures</a></li>
+            <li><a href="./home.php" class="nav-link px-4 link-secondary">Accueil</a></li>
+            <li><a href="./recherche.php" class="nav-link px-4 link-dark">Recherche de stage</a></li>
+            <li><a href="./candidature.php" class="nav-link px-4 link-dark">Mes candidatures</a></li>
         </ul>
 
         <div class="col-md-2 text-end">
-            <!--<button type="button" class="btn btn-outline-primary me-2">Login</button>
-            <button type="button" class="btn btn-primary">Sign-up</button>-->
+                <!--
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            <button type="button" class="btn btn-primary">Sign-up</button>
+                -->
             <form class="bi me-2" action="home.php" method="post">
-                <a href="#"><img class="bi me-2" src="assets\img\profil.jpg" alt="" width="60" height="60"></a>
+                <a href="./profil.php"><img class="bi me-2" src="assets\img\profil.jpg" alt="" width="60" height="60"></a>
 
                 <button class="btn btn-danger" type="submit" name="disconnect" value="true">Disconnect</button>
             </form>
