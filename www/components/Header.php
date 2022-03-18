8f1561733bce205a0f44319842fca384c3e5c893
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2022-03-18 10:09:44 +0100

    Rearangement des fichiers

--- www/components/Header.php
+++ www/components/Header.php
@@ -1,5 +1,5 @@
 <?php
-    /* // Mit en commentaire le temps de modifier les autres pages.
+    // Mit en commentaire le temps de modifier les autres pages.
     session_start();
 
     if (isset($_POST["disconnect"])) {
@@ -11,11 +11,11 @@
         header("Location: ./login.php");
         exit();
     }
-    */
+    
     ?>
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-white">
         <a href="#" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-            <img class="bi me-2" src="assets\img\logo.png" alt="" width="60" height="60">
+            <img class="bi me-2" src="../assets\img\logo.png" alt="" width="60" height="60">
         </a>
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
@@ -28,7 +28,7 @@
             <!--<button type="button" class="btn btn-outline-primary me-2">Login</button>
             <button type="button" class="btn btn-primary">Sign-up</button>-->
             <form class="bi me-2" action="home.php" method="post">
-                <a href="#"><img class="bi me-2" src="assets\img\profil.jpg" alt="" width="60" height="60"></a>
+                <a href="#"><img class="bi me-2" src="../assets\img\profil.jpg" alt="" width="60" height="60"></a>
 
                 <button class="btn btn-danger" type="submit" name="disconnect" value="true">Disconnect</button>
             </form>
--- www/pages/home.php
+++ www/pages/home.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/style/style.css">
+    <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/style/style.css">
 </head>
 
 <body>
-    <?php include 'header.php' ?>
+    <?php include '../components/header.php' ?>
 
     <div class="container-fluid">
         <div class="row justify-content-center">
@@ -61,7 +61,7 @@
         </div>
     </div>
 
-    <?php include 'footer.php' ?>
+    <?php include '../components/footer.php' ?>
 </body>
 
 </html>
