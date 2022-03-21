c576fdda8a0343c4680bd1e43fb282cb3f59a884
emilienpuech <emilien.puech@viacesi.fr>
2022-03-21 14:17:45 +0100

    push

--- www/bdd/controleur.php
+++ www/bdd/controleur.php
@@ -54,4 +54,8 @@ class Controleur{
     public function selectUsersSearch(&$sqlClient, &$data, &$nbRow, &$nbCol, $secondName, $firstName, $schoolYear){
         $this->_users->selectUsersSearch($this->$sqlClient, $this->$data, $this->$nbRow, $this->$nbCol, $secondName, $firstName, $schoolYear);
     }
+    public function select_wish_list_from_user(&$data,$user_id){
+        
+
+    }
 }
