3f5aa95d0d565b70d846f5285b828f05f19a0c67
Asarck <85520089+Asarck@users.noreply.github.com>
2022-03-21 15:20:34 +0100

    creation

--- www/pages/creation/entreprise.php
+++ www/pages/creation/entreprise.php
@@ -1,43 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/vendors/jqueryUI/jquery-ui.min.css">
-    <link rel="stylesheet" href="../assets/style/style.css">
-</head>
-
-<body>
-    <div style="text-align: -webkit-center;">
-        <div style="background: white;display: flex; width: 50%;justify-content: center;flex-direction: column;text-align: center;">
-            <form style="margin: 1em;">
-                <div class="mb-3">
-                    <label for="company" class="form-label">Nom de l'Entreprise</label>
-                    <input type="text" class="form-control" id="company" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-3">
-                    <label for="nbCESIStudent" class="form-label">Nombre de stagiaires</label>
-                    <input type="text" class="form-control" id="nbCESIStudent">
-                </div>
-                <div class="mb-3">
-                    <label for="eMail" class="form-label">Email Entreprise</label>
-                    <input type="email" class="form-control" id="eMail">
-                </div>
-                <div class="mb-3">
-                    <label for="descCompany" class="form-label">Description Entreprise</label>
-                    <textarea name="descCompany" class="form-control" id="descCompany" style="resize: none;" oninput='this.style.height = "";this.style.height = this.scrollHeight+ 5 + "px"' maxlength="500"></textarea>
-                </div>
-                <div class="mb-3 form-check" style="display: flex;justify-content: center;">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1" style="margin-left: 1em;">  Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
+<div style="text-align: -webkit-center;">
+    <div style="background: white;display: flex; width: 50%;justify-content: center;flex-direction: column;text-align: center;">
+        <form style="margin: 1em;">
+            <div class="mb-3">
+                <label for="company" class="form-label">Nom de l'Entreprise</label>
+                <input type="text" class="form-control" id="company" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="nbCESIStudent" class="form-label">Nombre de stagiaires</label>
+                <input type="text" class="form-control" id="nbCESIStudent">
+            </div>
+            <div class="mb-3">
+                <label for="eMail" class="form-label">Email Entreprise</label>
+                <input type="email" class="form-control" id="eMail">
+            </div>
+            <div class="mb-3">
+                <label for="descCompany" class="form-label">Description Entreprise</label>
+                <textarea name="descCompany" class="form-control" id="descCompany" style="resize: none;" oninput='this.style.height = "";this.style.height = this.scrollHeight+ 5 + "px"' maxlength="500"></textarea>
+            </div>
+            <div class="mb-3 form-check" style="display: flex;justify-content: center;">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1" style="margin-left: 1em;">  Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-</body>
-</html>
+</div>
