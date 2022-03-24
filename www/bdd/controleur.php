54c798afff9e7e8c174bcba88ff7f7cc9cbf5b53
emilienpuech <emilien.puech@viacesi.fr>
2022-03-24 15:04:32 +0100

    contreleur.php

--- www/bdd/controleur.php
+++ www/bdd/controleur.php
@@ -88,7 +88,7 @@ class Controleur{
     public function insertNewInternship($company, $internship, $StartDate, $EndDate, $WageMonth, $nbPlace, $descInternship, $locality){
         $this->_intership->insertNewInternship($this->mysqlClient, $company, $internship, $StartDate, $EndDate, $WageMonth, $nbPlace, $descInternship, $locality);
     }
-    public function delete_save($id_user,$id_internship){
-        $this->_users->delete_save($this->mysqlClient,$id_user,$id_internship);
+    public function delete_save($id_page){
+        $this->_users->delete_save($this->mysqlClient,$id_page);
     }
 }
