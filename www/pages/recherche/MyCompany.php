5672de15a10e0cca9f8f0650fafe8d9ea2f03d88
Asarck <85520089+Asarck@users.noreply.github.com>
2022-03-28 14:30:54 +0200

    entreprises

--- www/pages/recherche/MyCompany.php
+++ www/pages/recherche/MyCompany.php
@@ -6,30 +6,35 @@ require_once($path);
     $nbCol;
     $controleur = new Controleur();
     $controleur->selectUsersCompany($data, $nbRow, $nbCol);
+    if ($nbRow) {
+        echo '<br>
+        <div style="text-align: -webkit-center;">
+            <h3>Vos Entreprises</h3>
+        </div>';
+    }
     for ($j = 0; $j < $nbRow; $j++) {
         echo '<br>
         <div style="text-align: -webkit-center;">
             <h3>'.$data[$j]["company"].'</h3>
             <div style="display: flex; width: 45%;justify-content: center;flex-direction: column;text-align: center;">
                 <form style="margin: 1em;">
-                    <div>
+                    <div class="mb-3">
                         <input type="text" name="idCompany" id="idCompany" hidden value="'.$data[$j]["idCompany"].'">
                     </div>
-                    <div>
+                    <div class="mb-3">
                         <label for="company" class="form-label">Nom de l\'entreprise</label>
                         <input type="text" class="form-control" id="company" value="'.$data[$j]["company"].'">
                     </div>
-                    <div>
+                    <div class="mb-3">
                         <label for="eMail" class="form-label">EMail</label>
                         <input type="email" class="form-control" id="eMail" value="'.$data[$j]["eMail"].'">
                     </div>
-                    <div>
+                    <div class="mb-3">
                         <label for="descCompany" class="form-label">Description Entreprise</label>
                         <textarea name="descCompany" class="form-control" id="descCompany" style="resize: none;" oninput=\'this.style.height = "";this.style.height = this.scrollHeight+ 5 + "px"\' maxlength="400">'.$data[$j]["descCompany"].'</textarea>
                     </div>
-                    <div style="display: flex;justify-content: space-evenly;margin-top: 1em;">
-                        <li style="list-style: none;"></li>
-                        <div>
+                    <div class="mb-3" style="display: flex;justify-content: space-evenly;">
+                        <div class="mb-3">
                             <button type="submit" class="btn btn-primary">Submit</button>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
